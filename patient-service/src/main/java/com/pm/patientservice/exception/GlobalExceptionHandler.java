8b0cee5eb69a476d1e6eee47826d43f8278b3d96
Ashan <ashanvimodh241@gmail.com>
2025-10-27 23:47:25 +0530

    add log.warn(Email address already exists

--- patient-service/src/main/java/com/pm/patientservice/exception/GlobalExceptionHandler.java
+++ patient-service/src/main/java/com/pm/patientservice/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.pm.patientservice.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,6 +12,8 @@ import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidException(
@@ -22,6 +26,15 @@ public class GlobalExceptionHandler {
         });
 
         return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<String> handleEmailAlreadyExistsException(EmailAlreadyExistsException ex){
+
+        log.warn("Email address already exists: {}", ex.getMessage());
+        Map<String, String> errors = new HashMap<>();
+        errors.put("message", "Email address already exists" );
+        return ResponseEntity.badRequest().body(errors.toString());
     }
 
 }
