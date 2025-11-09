1e72cafae4f3fffbc7e30219175904292008311b
Ashan <ashanvimodh241@gmail.com>
2025-11-09 21:20:42 +0530

    auth token generating correctly

--- auth-service/src/main/java/com/pm/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/pm/authservice/controller/AuthController.java
@@ -1,6 +1,8 @@
 package com.pm.authservice.controller;
 
+import com.pm.authservice.dto.LoginRequestDTO;
 import com.pm.authservice.dto.LoginResponseDTO;
+import com.pm.authservice.service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,10 +14,17 @@ import java.util.Optional;
 
 @RestController
 public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
     @Operation(summary = "Generate token on user login")
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDTO> login(
-            @RequestBody LoginResponseDTO loginRequestDTO) {
+            @RequestBody LoginRequestDTO loginRequestDTO) {
 
         Optional<String> tokenOptional = authService.authenticate(loginRequestDTO);
 
