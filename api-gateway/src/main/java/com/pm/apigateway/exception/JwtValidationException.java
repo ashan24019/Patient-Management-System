7698df8b3cf778aff31029c6fd275d54385143ec
Ashan <ashanvimodh241@gmail.com>
2025-11-19 07:46:17 +0530

    auth service docs via gateway

---
+++ api-gateway/src/main/java/com/pm/apigateway/exception/JwtValidationException.java
@@ -0,0 +1,19 @@
+package com.pm.apigateway.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.reactive.function.client.WebClientRequestException;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@RestControllerAdvice
+public class JwtValidationException {
+    @ExceptionHandler(WebClientRequestException.class)
+    public Mono<Void> handleUnauthorizedException(ServerWebExchange exchange){
+        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        return exchange.getResponse().setComplete();
+    }
+
+
+}
