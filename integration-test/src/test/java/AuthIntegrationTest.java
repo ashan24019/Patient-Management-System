736d4d06e2e6e0948341511c599af52751625b81
Ashan <ashanvimodh241@gmail.com>
2025-11-25 20:40:07 +0530

    login integration testing with invalid credentials

--- integration-test/src/test/java/AuthIntegrationTest.java
+++ integration-test/src/test/java/AuthIntegrationTest.java
@@ -38,4 +38,26 @@ public class AuthIntegrationTest {
 
         System.out.println("Generated token: " + response.jsonPath().getString("token"));
     }
+
+    @Test
+    public void shouldReturnUnauthorizedOnInvalidLogin() {
+        // 1. Arrange
+        // 2. Act
+        // 3. Assert
+
+        String loginPayload = """
+                {
+                    "email": "invalid_user@test.com",
+                    "password": "wrongPassword123"
+                }
+                """;
+
+        given()
+                .contentType("application/json")
+                .body(loginPayload)
+                .when()
+                .post("/auth/login")
+                .then()
+                .statusCode(401);
+    }
 }
